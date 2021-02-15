aa48db9f0692b803a6727e34548d6be43a47f6fc
brandonworks <work@brandontorres.dev>
2021-02-14 16:49:01 -0800

    commit tree 5da2dcb18f26893b317c2fb65353d12e40f74599

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+#write a method that takes a string
+#method should return each word reversed in place
+sentence = "magic man"
+def reverse_each_word(sentence)
+  sentence.reverse
+end
+
+puts reverse_each_word(sentence)
